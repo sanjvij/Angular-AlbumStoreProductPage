cc3367501f346353aeec67822b7a745b3a913d79
sanjvij <62745844+sanjvij@users.noreply.github.com>
2020-04-26 13:51:18 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,7 @@ private _albumUrl = '../assets/album.json';
 
 getAlbum(id: number): Observable<Album>   {
   return this._http.get(this._albumUrl)
-  .map((response) => response.json() as Album);
+  .map((response) => <Album> response.json());
 }
 
 }
